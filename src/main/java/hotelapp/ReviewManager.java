980bb8f633da156351bf7c4274ef3f73b83f08f1
justin-pyne <jpyne.justin@gmail.com>
2024-10-21 15:45:55 -0700

    findReviews method

--- src/main/java/hotelapp/ReviewManager.java
+++ src/main/java/hotelapp/ReviewManager.java
@@ -1,20 +1,50 @@
 package hotelapp;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class ReviewManager {
     private Map<String, List<Review>> reviewMap = new HashMap<>();
     private Map<String, TreeSet<ReviewWithFrequency>> invertedIndex = new HashMap<>();
 
 
-    public String findReviews(String hotelId){
+    public ReviewManager(List<Review> reviews) {
+        for(Review review : reviews) {
+            if(!reviewMap.containsKey(review.getHotelId())) {
+                reviewMap.put(review.getHotelId(), new ArrayList<>());
+            }
+            reviewMap.get(review.getHotelId()).add(review);
+            invertedIndexReview(review);
+        }
+    }
 
+    public String findReviews(String hotelId){
+        if(!reviewMap.containsKey(hotelId)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Review> reviews = reviewMap.get(hotelId);
+
+        reviews.sort((r1, r2) -> {
+           int comp = r2.getDate().compareTo(r1.getDate());
+           if (comp == 0) {
+               return r1.getReviewId().compareTo(r2.getReviewId());
+           }
+           return comp;
+        });
+
+        StringBuilder result = new StringBuilder();
+
+        for (Review review : reviews) {
+            result.append(review.toString());
+        }
+        return result.toString();
     }
 
     public String findWord(String word){
+
+    }
+
+    private void invertedIndexReview(Review review) {
 
     }
 
